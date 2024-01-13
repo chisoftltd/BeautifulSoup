e7d091c1cd1732993d9ab29e0682804f6172e454
chisoftltd <chisoftltd@gmail.com>
2024-01-13 18:14:10 +0000

    Initial

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+# import lxml
+
+with open("website.html", encoding="utf8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title)
+# print(soup.title.string)
+print(soup)
+print(soup.prettify())
